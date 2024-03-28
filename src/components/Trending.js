8e3a9233247297340ecf0f22b4d0079e2719f91c
unknown <rikofsaputra000@gmail.com>
2024-03-28 21:51:33 +0700

    add API and connection

--- src/components/Trending.js
+++ src/components/Trending.js
@@ -1,12 +1,33 @@
+import { useEffect, useState } from "react";
 import { Card, Col, Container, Image, Row } from "react-bootstrap";
 import DuneImage from "../images/trending/dune.jpg";
-import EverthingImage from "../images/trending/everything.jpg";
-import InfiniteImage from "../images/trending/infinite.jpg";
-import JokerImage from "../images/trending/joker.jpg";
-import LightyearImage from "../images/trending/lightyear.jpg";
-import MorbiusImage from "../images/trending/morbius.jpg";
+// import EverthingImage from "../images/trending/everything.jpg";
+// import InfiniteImage from "../images/trending/infinite.jpg";
+// import JokerImage from "../images/trending/joker.jpg";
+// import LightyearImage from "../images/trending/lightyear.jpg";
+// import MorbiusImage from "../images/trending/morbius.jpg";
+import axios from "axios"
+
 
 const Trending = () => {
+
+  const [movies, setMovies] = useState([])
+  useEffect(() => {
+    axios.get(`${process.env.REACT_APP_BASE_URL}`, {
+      params: {
+         api_key: process.env.REACT_APP_TMDB_KEY
+      }
+    }).then((res) => {
+      // console.log("inihasilnya ", res.data.results)
+      setMovies(res.data.results)
+    }).catch(err => {
+      console.log(err)
+    })
+  },[])
+
+
+
+
   return (
     <div>
       <Container>
@@ -14,108 +35,29 @@ const Trending = () => {
         <h1 className="mt-4 text-white ">TRENDING MOVIES</h1>
         <br/>
         <Row>
-          <Col md={4} className="MovieWrapper" id="tranding">
+
+          {movies.map((result, index) => {
+  
+            return(
+               <Col md={4} className="MovieWrapper" id="tranding" key={index}>
             <Card className=" MovieImage">
-              <Image src={DuneImage} alt="dune" className="trandingImage"></Image>
+              <Image src={`${process.env.REACT_APP_IMG_URL}/${result.poster_path}`} alt="tust" className="trandingImage"></Image>
               <div className="bg-dark">
                 <diV className="p-2 m-1 text-white">
-                  <Card.Title className=" text-center">Card title</Card.Title>
+                  <Card.Title className=" text-center">{result.title}</Card.Title>
                   <Card.Text className="text-left">
-                    This is a wider card with supporting text below as a natural
-                    lead-in to additional content.
+                    {result.overview}.slice(50)
                   </Card.Text>
                   <Card.Text className="text-left">
-                    Last updated 3 mins ago
-                  </Card.Text>
-                </diV>
-              </div>
-            </Card>
-          </Col>
-          <Col md={4} className="MovieWrapper">
-            <Card className=" MovieImage">
-              <Image src={EverthingImage} alt="dune" className="trandingImage"></Image>
-              <div className="bg-dark">
-                <diV className="p-2 m-1 text-white">
-                  <Card.Title className=" text-center">Card title</Card.Title>
-                  <Card.Text className="text-left">
-                    This is a wider card with supporting text below as a natural
-                    lead-in to additional content.
-                  </Card.Text>
-                  <Card.Text className="text-left">
-                    Last updated 3 mins ago
+                    {result.release_date}
                   </Card.Text>
                 </diV>
               </div>
             </Card>
-          </Col>
-          <Col md={4} className="MovieWrapper">
-            <Card className="MovieImage">
-              <Image src={InfiniteImage} alt="dune" className="trandingImage"></Image>
-              <div className="bg-dark ">
-                <diV className="p-2 m-1 text-white">
-                  <Card.Title className=" text-center">Card title</Card.Title>
-                  <Card.Text className="text-left">
-                    This is a wider card with supporting text below as a natural
-                    lead-in to additional content.
-                  </Card.Text>
-                  <Card.Text className="text-left">
-                    Last updated 3 mins ago
-                  </Card.Text>
-                </diV>
-              </div>
-            </Card>
-          </Col>
-          <Col md={4} className="MovieWrapper">
-            <Card className="MovieImage">
-              <Image src={JokerImage} alt="dune" className="trandingImage"></Image>
-              <div className="bg-dark ">
-                <diV className="p-2 m-1 text-white">
-                  <Card.Title className=" text-center">Card title</Card.Title>
-                  <Card.Text className="text-left">
-                    This is a wider card with supporting text below as a natural
-                    lead-in to additional content.
-                  </Card.Text>
-                  <Card.Text className="text-left">
-                    Last updated 3 mins ago
-                  </Card.Text>
-                </diV>
-              </div>
-            </Card>
-          </Col>
-          <Col md={4} className="MovieWrapper">
-            <Card className="MovieImage">
-              <Image src={LightyearImage} alt="dune" className="trandingImage"></Image>
-              <div className="bg-dark ">
-                <diV className="p-2 m-1 text-white">
-                  <Card.Title className=" text-center">Card title</Card.Title>
-                  <Card.Text className="text-left">
-                    This is a wider card with supporting text below as a natural
-                    lead-in to additional content.
-                  </Card.Text>
-                  <Card.Text className="text-left">
-                    Last updated 3 mins ago
-                  </Card.Text>
-                </diV>
-              </div>
-            </Card>
-          </Col>
-          <Col md={4} className="MovieWrapper">
-            <Card className="MovieImage">
-              <Image src={MorbiusImage} alt="dune" className="trandingImage"></Image>
-              <div className="bg-dark ">
-                <diV className="p-2 m-1 text-white">
-                  <Card.Title className=" text-center">Card title</Card.Title>
-                  <Card.Text className="text-left">
-                    This is a wider card with supporting text below as a natural
-                    lead-in to additional content.
-                  </Card.Text>
-                  <Card.Text className="text-left">
-                    Last updated 3 mins ago
-                  </Card.Text>
-                </diV>
-              </div>
-            </Card>
-          </Col>
+          </Col> 
+            )
+          })}
+
         </Row>
       </Container>
     </div>
